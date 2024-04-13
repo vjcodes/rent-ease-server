5ded7177b31473cad6774196c508fab3482b5353
vjcodes <vinayak.kris@gmail.com>
2024-04-14 03:45:19 +0530

    -post route changes

--- routes/post.route.js
+++ routes/post.route.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get("/", getPosts);
 router.get("/:id", getPost);
-router.post("/", verifyToken, addPost);
+router.post("/", addPost);
 router.put("/:id", verifyToken, updatePost);
 router.delete("/:id", verifyToken, deletePost);
 
